af7903c3aac6517a4c4db0199f2cb9975a0bd1ed
Dmitry Nemkov <xeqlol@gmail.com>
2017-10-26 19:42:56 +0500

    woops, fixed !commands

--- src/lib/commands/commands.py
+++ src/lib/commands/commands.py
@@ -1,6 +1,7 @@
 import src
 
 
+# one of defaults bot's commands
 def commands():
-    available_commands = ', '.join(str(command) for command in src.lib.commands.__command_headers.command_headers)
+    available_commands = ', '.join(str(command) for command in src.lib.commands._command_headers.command_headers)
     return 'Available commands: %s' % available_commands
